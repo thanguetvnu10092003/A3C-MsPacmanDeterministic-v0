b43ac673a9ecb82caaa8d46c0e4302149662f52a
LeToanThang <toanthangvietduc@gmail.com>
2025-12-27 17:30:19 +0700

    Fix: Update Gymnasium env name from MsPacmanDeterministic-v0 to ALE/MsPacman-v5

--- utils/Make_Env.py
+++ utils/Make_Env.py
@@ -4,6 +4,8 @@ import ale_py
 gym.register_envs(ale_py)
 
 def make_env(mode):
-    env = gym.make('MsPacmanDeterministic-v0', render_mode=mode)
+    # MsPacmanDeterministic-v0 đã được đổi tên trong Gymnasium mới
+    # Sử dụng ALE/MsPacman-v5 với frameskip=4 (tương đương Deterministic)
+    env = gym.make('ALE/MsPacman-v5', render_mode=mode, frameskip=4)
     env = PreprocessAtari(env, height=42, width=42, crop=lambda img: img, dim_order='pytorch', color=False, n_frames=20)
     return env
